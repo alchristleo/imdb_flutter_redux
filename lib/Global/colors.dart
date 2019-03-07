85e246c6816d561fb6d4e36a68c91470beefc10d
Alchrist <alchristleo96@gmail.com>
2019-03-07 17:55:45 +0700

    Create loading screen and setting up navigator, utils, color

---
+++ lib/Global/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:imdb_flutter_redux/Utils/colorParser.dart';
+
+Color colorWhite = HexColor("#ffffff");
+Color colorBlack = HexColor("#000000");
+Color colorMinionYellow = HexColor("#f3ce13");
