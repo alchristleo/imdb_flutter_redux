b31cea816098422935bd46223b53addf1c6e28d7
Alchrist <alchristleo96@gmail.com>
2019-03-14 01:20:56 +0700

    redo with gitignore

---
+++ presentation/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:shared/index.dart';
+import 'package:flutter/material.dart';
+import 'package:imdb_flutter_redux/components/loading.dart';
+
+var routes = <String, WidgetBuilder>{
+    // LIST ALL ROUTES
+};
+
+void main() => runApp(new MaterialApp(
+    theme:
+        ThemeData(primaryColor: colorMinionYellow, accentColor: colorWhite),
+    debugShowCheckedModeBanner: false,
+    home: LoadingScreen(),
+    routes: routes));
